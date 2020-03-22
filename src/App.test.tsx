bbf2204832e8fdc6b6bb62ad1baa3e1b676192e0
hemanditwiz <hemanditwiz@gmail.com>
2020-03-22 20:44:12 +0530

    intial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
@@ -12,4 +12,12 @@ test('renders learn react link', () => {
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
+});
+
+test("Sample test", () => {
+  expect(1 + 2).toBe(3);
+});
+
+test("Sample test", () => {
+  expect(1 + 2).toBe(3);
 });
